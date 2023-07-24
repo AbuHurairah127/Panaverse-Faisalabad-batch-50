9961aff3b8602c9cbcab1564a30bba3b1aef72a5
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2023-07-24 10:25:48 +0500

    class one code completed

--- typescript/class-01/index.ts
+++ typescript/class-01/index.ts
@@ -1,1 +1,20 @@
 console.log("Abu Hurairah, Naveed Sarwar"); //To show user output (on console)
+
+var userName = "Abu Hurairah"; // "var" is a reserved keyword for declaring a variable. "userName" is the name of variable
+//And after equal in inverted commas is the text we want to store.
+console.log(userName); // reading the variable and printing it's value
+
+var userName2: string = "Naveed"; //Restricting the variable to only store "string" data.
+console.log(userName2);
+
+var num1: number = 2; //variable for number values "can contain both integer or float value"
+var num2: number = 4;
+
+/*In typescript, 
+"+" is used as addition operator
+"-" is used as subtraction operator
+"*" is used as multiplication operator
+"/" is used as division operator
+"=" is used as assignment operator (in maths it is used as equal to operator but in typescript it doesn't.) */
+var result: number = num1 + num2; //Adding two variables
+console.log(result);
