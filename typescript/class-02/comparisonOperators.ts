ce402e4151a07a5ec3a81eb4be74ca28d9759145
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-02 04:25:10 -0700

    comparison operator done

---
+++ typescript/class-02/comparisonOperators.ts
@@ -0,0 +1,26 @@
+var num1: number = 10;
+var num2: number = 20;
+
+// Comparison operator returns BOOLEAN type value.
+
+// less than operator
+var result: boolean = num2 < num1;
+
+// greater than operator
+var result2: boolean = num1 > num2;
+
+// equal to operator 
+//only value check.
+var result3: boolean = num1 == num2;
+// value check and datatype check.
+var result4: boolean = num1 === num2;
+
+//Greater than and equal to operator
+var result5: boolean = num1 >= num2;
+
+//Less than and equal to operator
+var result5: boolean = num1 <= num2;
+
+//not less than operator
+var result6: boolean = num1! < =num2
+
