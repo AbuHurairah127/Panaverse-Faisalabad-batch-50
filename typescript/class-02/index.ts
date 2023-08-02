42e1b7c6c53695630223557ac716fe3173c38a73
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-02 11:43:41 -0700

    string varibale and concatination

---
+++ typescript/class-02/index.ts
@@ -0,0 +1,12 @@
+// Initializing String type variables
+var userName: string = "Abu Hurairah";
+var userName2: string = 'Naveed';
+
+var userName3: string = `Naveed`;//Specifically this one is called template literals. As it involves different features.
+
+// Let's try to concatenate two strings
+//Without template literals
+var firstName: string = "Muhammad"
+var lastName: string = "Ali"
+var fullName: string = firstName + " " + lastName
+console.log("fullName:", fullName)
