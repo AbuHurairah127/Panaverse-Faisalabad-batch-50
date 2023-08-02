93b8d2dc0d7c333109f929c51548591bb146affe
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-02 11:49:58 -0700

    minor update and string work about done

--- typescript/class-02/index.ts
+++ typescript/class-02/index.ts
@@ -6,7 +6,15 @@ var userName3: string = `Naveed`;//Specifically this one is called template lite
 
 // Let's try to concatenate two strings
 //Without template literals
-var firstName: string = "Muhammad"
-var lastName: string = "Ali"
-var fullName: string = firstName + " " + lastName
-console.log("fullName:", fullName)
+var firstName: string = "Muhammad";
+var lastName: string = "Ali";
+var fullName: string = firstName + " " + lastName;
+console.log("fullName:", fullName);
+// With template literals
+var firstName1: string = `Ahmad`;
+var lastName1: string = `Ali`;
+var fullName1: string = `${firstName1} ${lastName1}`;//using template literals we can use variable any where in the string, just using ${variableName}. The dollar sign help compiler to understand that here is the start of javascript/typescript
+console.log("fullName1:", fullName1);
+//Another example
+console.log(`Assalam u Alakium ${fullName}, How are you???`)
+
