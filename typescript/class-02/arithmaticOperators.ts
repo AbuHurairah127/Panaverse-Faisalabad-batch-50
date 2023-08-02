91533056038786a7b8ce4c0b7a8eb63fa6606143
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-02 04:16:13 -0700

    arithmatic operator and logical operator done

---
+++ typescript/class-02/arithmaticOperators.ts
@@ -0,0 +1,15 @@
+//ARITHMETIC OPERATORS:
+var num1: number = 10;
+var num2: number = 20;
+// addition
+var sum = num1 + num2;
+//subtraction
+var minus = num1 - num2;
+// multiplication
+var product = num1 * num2;
+// exponent (raise to power ^)
+var exponent = 2 ** 5;
+// division
+var division = num2 / num1;
+//division (remainder)
+var remainder = num2 / 3;
---
+++ typescript/class-02/logicalOperators.ts
@@ -0,0 +1,32 @@
+// OR Operator ||
+/*
+OR operator help table
+input1      input2      result
+true        true         true
+true        false        true
+false       true         true
+false       false        false
+ - If ONE is TRUE then ALL TRUE.
+ */
+
+
+//AND Operator &&
+/*
+AND operator table
+input1      input2      result
+true        true         true
+true        false        false
+false       true         false
+false       false        false
+- If ONE is FALSE then ALL FALSE.
+*/
+
+
+//NOT operator !
+/* 
+NOT operator table
+input      result
+true        false
+false       true
+- Input will be inverted.
+*/
