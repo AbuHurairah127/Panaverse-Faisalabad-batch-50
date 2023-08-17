afd55840403af1432db7096b10add9b31429dc2e
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-17 19:30:28 +0500

    function with parameters added

--- typescript/class-04/index.ts
+++ typescript/class-04/index.ts
@@ -16,3 +16,17 @@ function sum() {
 sum();
 // Now the above given example is of a function that sum two number and print the value of the sum.
 // Now whenever we call the function it performs the specific task
+// -----------------------------------------------------------------------------------------------------------------------------------
+
+// Function with parameters
+// Use Case: Now let's say we have to perform sum of two numbers but we want the number changeable. So the solution is functions with parameters
+// For this type of functions we can send data to function wherever we call it.
+
+function sum2Numbers(num1: number, num2: number) {
+  let sum: number = num1 + num2;
+  console.log(sum);
+}
+
+sum2Numbers(30, 90);
+// Now the above given example is of a function that takes two numbers and add them and print them
+//  -------------------------------------------------------------------------------------------------------------------------------------
