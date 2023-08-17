4f6f0c35f42074c7b7826903763bac01a253c6b1
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2023-08-17 19:07:38 +0500

    simple funciton done

---
+++ typescript/class-04/index.ts
@@ -0,0 +1,18 @@
+// Functions are used to make the code reusable.
+// We can write a specific set of code in function body and call it any where in the file when ever we want.
+
+// !Syntax
+// function name(params: type) {
+//      Body (Block of code)
+// }
+
+function sum() {
+  let num1: number = 10;
+  let num2: number = 20;
+  let sum: number = num1 + num2;
+  console.log(sum);
+}
+
+sum();
+// Now the above given example is of a function that sum two number and print the value of the sum.
+// Now whenever we call the function it performs the specific task
