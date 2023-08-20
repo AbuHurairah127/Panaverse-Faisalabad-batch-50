e6379621ea406e0207912f47eb1a8186b80fa002
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-08-20 06:21:51 +0500

    function with return

--- typescript/class-04/index.ts
+++ typescript/class-04/index.ts
@@ -30,3 +30,19 @@ function sum2Numbers(num1: number, num2: number) {
 sum2Numbers(30, 90);
 // Now the above given example is of a function that takes two numbers and add them and print them
 //  -------------------------------------------------------------------------------------------------------------------------------------
+
+// Function with return
+// Use Case: Now let's say we have to perform sum of two numbers but we want the result outside the function so that we can perform more logic on the result
+// We have to return the value and one function can return only one value or variable
+// We can use the returned value:
+//  - Store in a variable.
+//  - Directly show in output.
+//  - Perform some logic or calculations.
+function sum3(num1: number, num2: number) {
+  let sum: number = num1 + num2;
+  return sum;
+}
+
+let resultSum: number = sum3(56784, -90); // This will store the value into resultSum variable
+let result: number = sum3(12354, 7981) + 14; //Perform the logic or calculations
+console.log(sum3(36578, 7842)); //Directly show output
