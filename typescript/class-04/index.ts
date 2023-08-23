136a9cf5b94e9aebd0c3f0a8fdf56a2279d0c21f
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-08-23 23:23:30 +0500

    functions completed

--- typescript/class-04/index.ts
+++ typescript/class-04/index.ts
@@ -31,7 +31,7 @@ sum2Numbers(30, 90);
 // Now the above given example is of a function that takes two numbers and add them and print them
 //  -------------------------------------------------------------------------------------------------------------------------------------
 
-// Function with return   
+// Function with return
 // Use Case: Now let's say we have to perform sum of two numbers but we want the result outside the function so that we can perform more logic on the result
 // We have to return the value and one function can return only one value or variable
 // We can use the returned value:
@@ -46,3 +46,4 @@ function sum3(num1: number, num2: number) {
 let resultSum: number = sum3(56784, -90); // This will store the value into resultSum variable
 let result: number = sum3(12354, 7981) + 14; //Perform the logic or calculations
 console.log(sum3(36578, 7842)); //Directly show output
+sum2Numbers(sum3(3, 4), 4); //function calling function as parameter and now the inner function means sum3 function will be called first and then sum2Numbers functions will be called.
