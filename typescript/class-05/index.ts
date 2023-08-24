9dcc48ac88d2e3cfa3b7cf03a1a4b095d1b32c7e
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-08-24 09:46:52 +0500

    Arrays added

---
+++ typescript/class-05/index.ts
@@ -0,0 +1,9 @@
+let arr = ["name", 10000, false];
+// Now let's say that we have to store the names of students of a class of strength 500
+// So, the first solution comes in our mind is make a separate variable for each name but it's too much problematic work.
+// Here is a new tool that we can use to do this named as array.
+
+let names = ["Abu Hurairah", "Zia Khan", "Naveed Rana"];
+// The above given array is of any type means we can push data in to the array of any type like undefined, number, string etc.
+
+let typeStrictArray: number[] = [2, 4, 5, 7, 9, 7, 8, 2, 1, 4, 8, 12, 85, 5, 7];
