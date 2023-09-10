0eeeb644b24f55f35e54271b3e2756acab754c08
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-09-10 22:24:22 +0500

    class 9 code added

---
+++ typescript/class-09/index.ts
@@ -0,0 +1,73 @@
+// console.log("Start "); //1
+
+// function printInFunction() {
+// const time = new Date();
+// // const _time = time.toLocaleTimeString();
+// console.log("🚀 ~ file: index.ts:5 ~ printInFunction ~ time:", time);
+// }
+
+// let count = 0; //0//1//2//3
+// const interval_id = setInterval(() => {
+//   printInFunction();
+//   count++;
+//   if (count === 3) {
+//     clearInterval(interval_id);
+//   }
+// }, 2000);
+
+// console.log("end"); //2
+// // A
+// // console.log("Start");
+
+// // setTimeout(() => {
+// //   console.log("In set time out");
+// // }, 1000);
+
+// // console.log("End");
+
+// let car: {} = {
+//   model: "civic",
+//   brand: "Honda",
+// };
+
+// class Car {
+//   model: string = "";
+//   brand: string = "";
+//   year?: number = 0;
+
+//   constructor(_model: string, _brand: string, _year: number) {
+//     this.model = _model;
+//     this.brand = _brand;
+//     this.year = _year;
+//   }
+// }
+
+// let newCar = new Car("S Class", "Mercedes", 2023);
+// let newCar2 = new Car("A8L", "Audi", 2022);
+// console.log("🚀 ~ file: index.ts:44 ~ newCar:", newCar);
+// console.log("🚀 ~ file: index.ts:45 ~ newCar2:", newCar2);
+
+// let car = new Car();
+
+// car.brand = "BMW";
+// car.model = "M4 Competition";
+// console.log("car", car);
+
+// let car2 = new Car();
+// car2.brand = "Audi";
+// car2.model = "A8L";
+
+// console.log("car2", car2);
+
+// class Teacher {
+//   name: string = "";
+//   age: number = 0;
+//   salary: number = 0;
+//   grade: string = "";
+//   constructor(_name: string) {
+//     this.name = _name;
+//   }
+// }
+
+// let abuhurairah = new Teacher("Abu Hurairah");
+// console.log("🚀 ~ file: index.ts:73 ~ abuhurairah:", abuhurairah);
