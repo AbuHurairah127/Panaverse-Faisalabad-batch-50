1a844739904a46babf6bb4d3210d3a232e93434e
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-09-10 22:26:52 +0500

    class-09-a code

---
+++ typescript/class-09-a/index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+inquirer
+    .prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter the first number?",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter the second number?",
+    },
+    {
+        name: "operation",
+        type: "list",
+        message: "Enter the operation?",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+])
+    .then((answer) => {
+    console.log(answer);
+    switch (answer.operation) {
+        case "Addition":
+            console.log(answer.num1 + answer.num2);
+            break;
+        case "Subtraction":
+            console.log(answer.num1 - answer.num2);
+            break;
+        default:
+            break;
+    }
+});
