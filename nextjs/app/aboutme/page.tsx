f8c57b59156791d9e7a4001549f133312502ef13
abuhurairah127 <abuhurairahMuhammadShafique@gmail.com>
2023-10-17 21:34:38 +0500

    html css lecture added

---
+++ nextjs/app/aboutme/page.tsx
@@ -0,0 +1,9 @@
+function AboutMe() {
+  return (
+    <div>
+      <p>About Me</p>
+    </div>
+  );
+}
+
+export default AboutMe;
---
+++ nextjs/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <p className="text-gray-500 bg-red-700">pag</p>e
+    </div>
+  );
+}
